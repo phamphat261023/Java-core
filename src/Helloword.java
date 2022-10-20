c1a7fa7057125dd0d13289d50a742863476835ae
PhamNgocPhat <114579161+phamphat261023@users.noreply.github.com>
2022-10-20 16:14:02 +0700

    java 1

---
+++ src/Helloword.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Helloword {
+
+    public static void main(String[] args){
+        String thongBao = "Xin chao";
+        String ten = "Phat";
+        String diaChi = "Ha Noi";
+
+        System.out.println("Thong bao: " + thongBao+ " " + ten + " " + diaChi);
+    }
+}
